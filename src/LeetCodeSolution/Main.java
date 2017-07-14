9411784d02f7926dc3053d43953a4c6728b75514
TAOXUEYU <362922388@qq.com>
2017-07-14 16:52:49 +0800

    update

--- src/LeetCodeSolution/Main.java
+++ src/LeetCodeSolution/Main.java
@@ -1,8 +1,4 @@
-import java.util.List;
-import java.util.PriorityQueue;
-
 public class Main {
-
     public static void main(String[] args) {
         Solution326 s = new Solution326();
         System.out.println(s.isPowerOfThree(27));
--- src/LeetCodeSolution/Solution507.java
+++ src/LeetCodeSolution/Solution507.java
@@ -1,3 +1,5 @@
+package LeetCodeSolution;
+
 import java.util.ArrayList;
 import java.util.List;
 
