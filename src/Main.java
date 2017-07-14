97e133b1c2ffd8207ad53a9a19ccc1d08d650269
TAOXUEYU <362922388@qq.com>
2017-07-14 11:09:59 +0800

    update

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,8 @@ import java.util.PriorityQueue;
 public class Main {
 
     public static void main(String[] args) {
-        Solution378 s = new Solution378();
-        //System.out.println(Math.pow(Integer.MAX_VALUE,0.25));
-        //.out.println(s.hammingWeight(11));
-        //PriorityQueue<Integer> res = new PriorityQueue<>();
-        //res.offer(4);res.offer(1);res.offer(3);res.offer(2);
-        //System.out.println(res.toString());
-
+        Solution9 s = new Solution9();
+        System.out.println(s.isPalindrome(123454321));
 
     }
 }
