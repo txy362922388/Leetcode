9d1bd2713d9f6148a041301d64b60c46193b5d39
TAOXUEYU <362922388@qq.com>
2017-07-14 14:43:42 +0800

    update

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ import java.util.PriorityQueue;
 public class Main {
 
     public static void main(String[] args) {
-        Solution9 s = new Solution9();
-        System.out.println(s.isPalindrome(123454321));
+        Solution507 s = new Solution507();
+        System.out.println(s.checkPerfectNumber(9999999));
 
     }
 }
