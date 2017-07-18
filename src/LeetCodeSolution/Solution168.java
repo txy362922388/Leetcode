b3ff63add5e81c28f7eba8d0a6fffe278670525d
TAOXUEYU <362922388@qq.com>
2017-07-18 22:31:37 +0800

    Merge branch 'master' of https://github.com/txy362922388/Leetcode
    # Conflicts:
    #	.idea/workspace.xml

--- src/LeetCodeSolution/Solution168.java
+++ src/LeetCodeSolution/Solution168.java
@@ -4,4 +4,22 @@ package LeetCodeSolution;
  * Created by EVAN on 2017/7/18.
  */
 public class Solution168 {
+    String table = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public String convertToTitle(int n) {
+        if (n <= 0)
+            return "";
+        StringBuilder sb = new StringBuilder();
+        while (n != 0) {
+            int remain = n % 26;
+            if (remain == 0) {
+                sb.append('Z');
+                n = n / 26 - 1;
+                continue;
+            }
+            sb.append(table.charAt(remain - 1));
+            n = n / 26;
+        }
+        sb.reverse();
+        return sb.toString();
+    }
 }
--- src/LeetCodeSolution/Solution400.java
+++ src/LeetCodeSolution/Solution400.java
@@ -4,4 +4,13 @@ package LeetCodeSolution;
  * Created by EVAN on 2017/7/18.
  */
 public class Solution400 {
+    public int findNthDigit(int n) {
+        StringBuilder sb = new StringBuilder();
+        int i = 1;
+        while (i <= n) {
+            sb.append(i);
+            i++;
+        }
+        return sb.charAt(n) - '0';
+    }
 }
